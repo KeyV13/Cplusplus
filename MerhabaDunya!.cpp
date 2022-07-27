f7b992757f6c8260c81206eb083eb348ef61a25c
Vedat <109217403+KeyV13@users.noreply.github.com>
2022-07-27 16:01:22 +0300

    Add files via upload

---
+++ MerhabaDunya!.cpp
@@ -0,0 +1,11 @@
+﻿// MerhabaDunya!.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "I am Learning C++";
+
+}
