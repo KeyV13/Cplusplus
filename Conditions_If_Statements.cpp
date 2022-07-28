3abc47aeb4f4e3998dd3ae2802a0326dbf1670c4
Vedat <109217403+KeyV13@users.noreply.github.com>
2022-07-28 12:16:59 +0300

    Add files via upload

---
+++ Conditions_If_Statements.cpp
@@ -0,0 +1,51 @@
+﻿// Conditions_If_Statements.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //if
+    int x = 20;
+    int y = 18;
+    if (x > y) {
+        cout << "x,y'den buyuk.";
+    }
+
+    cout << "\n\n";
+
+    // if else
+    int zaman = 20;
+    if(zaman < 18) {
+        cout << "Iyi gunler.";
+    } else{
+      cout << "Iyi aksamlar.";
+    }
+
+    cout << "\n\n";
+
+
+    // else if
+    int time = 22;
+    if (time < 10) {
+        cout << "Gunaydin.";
+    }
+    else if (time < 20) {
+        cout << "Iyi gunler.";
+    }
+    else {
+        cout << "Iyi aksamlar.";
+    }
+
+    cout << "\n\n";
+
+    // Birden çok kod satırını tek bir satırla değiştirmek için kullanılabilir
+    // degisken = (koşul) ? ifade Doğru: ifade Yanlış;
+    int timer = 20;
+    string result = (timer > 18) ? "Iyi gunler." : "Iyi aksamlar.";
+    cout << result;
+
+    return 0;
+}
+
---
+++ Switch.cpp
@@ -0,0 +1,42 @@
+﻿// Switch.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+// break anahtar bloğundaki kodun geri kalanının yürütülmesini "yok saydığı" için çok fazla yürütme süresi kazandırabilir.
+// Anahtar default, büyük/küçük harf eşleşmesi olmadığında çalıştırılacak bazı kodları belirtir
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int gun = 4;
+
+	switch (gun)
+	{
+	  case 1:
+		  cout << "Pazartesi"; 
+          break;
+      case 2:
+          cout << "Sali";
+          break;
+      case 3:
+          cout << "Carsamba";
+          break;
+      case 4:
+          cout << "Persembe";
+          break;
+      case 5:
+          cout << "Cuma";
+          break;
+      case 6:
+          cout << "Cumartesi";
+          break;
+      case 7:
+          cout << "Pazar";
+          break;
+
+	default:
+        cout << "Hafta sonunu dort gozle bekliyorum.";
+		break;
+	}
+
+    return 0;
+}
