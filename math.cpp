84c79992f3cdbc7d27654ba3254c00bc3d0b8ee8
Vedat <109217403+KeyV13@users.noreply.github.com>
2022-07-28 12:16:21 +0300

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,41 @@
+﻿// math.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+// sqrt(Kare kök), round(sayı yuvarlar) ve log (doğal logaritma) gibi diğer işlevler <cmath>başlık dosyasında bulunabilir.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	cout << sqrt(64) << endl;
+	cout << round(2.6) << endl;
+	cout << log(2) << endl;
+
+
+	/*
+    Function                Tanım
+    
+    abs(x)              x'in mutlak değerini döndürür
+    acos(x)             x'in ark kosinini döndürür
+    asin(x)             x'in ark sinüsünü döndürür
+    atan(x)             x'in arctangent'ini döndürür
+    tcmb(x)             x'in küp kökünü döndürür
+    ceil(x)             en yakın tamsayıya yuvarlanmış x değerini döndürür
+    cos(x)              x'in kosinüsünü döndürür
+    cosh(x)             x'in hiperbolik kosinüsünü döndürür
+    exp(x)              e^x değerini döndürür
+    expm1(x)            e^x -1 değerini döndürür
+    fabs(x)             Kayan bir x'in mutlak değerini döndürür
+    fdım(x, y)          x ve y arasındaki pozitif farkı döndürür
+    floor(x)            en yakın tamsayıya yuvarlanmış x değerini döndürür
+    hipotenüs(x, y)     Ara taşma veya alt akış olmadan sqrt'yi (x2 + y2) döndürür
+    fma(x, y, z)        hassasiyeti kaybetmeden x * y + z değerini döndürür
+    fmax(x, y)          Kayan bir x ve y'nin en yüksek değerini döndürür
+    fmin(x, y)          Kayan bir x ve y'nin en düşük değerini döndürür
+    fmod (x, y)         x / y'nin kayan nokta kalanını döndürür
+    pow(x, y)           x değerini y'nin gücüne döndürür
+    sin(x)              x'in sinüsünü döndürür (x radyan cinsinden)
+    sinh(x)             bir çift değerin hiperbolik sinüsünü döndürür
+    tan(x)              Bir açının tanjantını döndürür
+    tanh(x)             bir çift değerin hiperbolik tanjantını döndürür    */
+}
