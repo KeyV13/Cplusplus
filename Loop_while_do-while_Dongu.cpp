6ab98a262d8cfcda028729a1d1eb408c749edbbb
Vedat <109217403+KeyV13@users.noreply.github.com>
2022-07-28 12:22:18 +0300

    Add files via upload

---
+++ Loop_while_do-while_Dongu.cpp
@@ -0,0 +1,27 @@
+﻿// Loop_while_do-while_Dongu.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+	// Aşağıdaki örnekte, bir değişken ( i ) 5'ten küçük olduğu sürece döngüdeki kod tekrar tekrar çalışacaktır.
+	int i = 0;
+	while (i < 5) {
+		std::cout << i << "\n";
+		i++;   // Not: Koşulda kullanılan değişkeni artırmayı unutmayın, aksi takdirde döngü asla bitmez!
+	}
+
+	std::cout << "\n*****\n";
+
+	// Aşağıdaki örnek bir do/whiledöngü kullanmaktadır. Kod bloğu koşul test edilmeden önce yürütüldüğünden, 
+	// koşul yanlış olsa bile döngü her zaman en az bir kez yürütülür.
+
+	int j = 0;
+	do {
+		std::cout << j << "\n";
+		j++;
+	} while (j < 5);
+
+	return 0;
+}
---
+++ loop_for.cpp
@@ -0,0 +1,63 @@
+﻿// loop_for.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+// Bir kod bloğunda tam olarak kaç kez döngü yapmak istediğinizi biliyorsanız,for öngüyü kullanın.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* 
+    for(ifade 1; ifade 2; ifade 3) {
+       // yürütülecek kod bloğu
+     }   
+     
+    İfade 1 , kod bloğunun yürütülmesinden önce (bir kez) yürütülür.
+    İfade 2 , kod bloğunun yürütülmesi için koşulu tanımlar.
+    Kod bloğu yürütüldükten sonra (her seferinde) İfade 3 yürütülür.  */
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << "\n";
+    }
+
+    for(int i = 0; i < 3;i++) {
+        cout << "Yazilim" << "\n";
+    }
+
+    // döngüden çıkmak için de break kullanılabilir
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+            break;
+        }
+        cout << i << "\n";
+    }
+
+    // Belirtilen continue bir koşul oluşursa, ifade bir yinelemeyi (döngüde) keser ve döngüdeki bir sonraki yinelemeyle devam eder.
+    for (int i = 0; i < 10; i++) {
+        // Eğer i 6'ya eşitse,
+        // bir sonraki yinelemeye devam et
+        if (i == 4) {
+            continue;
+        }
+        // aksi halde i değerini yazdır
+        cout << i << "\n";
+    }
+
+    int k = 10;
+    if (k == 10)
+    {
+        //  İlk if ifadesi
+        if (k < 15)
+            cout << "k 15 ten kuccuk\n";
+
+        // İç içe geçmiş - if ifadesi
+        // Yalnızca yukarıdaki ifade yürütülürse yürütülür
+        // doğrudur
+        if (k < 12)
+            cout << "k 12 den de kucuk\n";
+        else
+            cout << "k 15 teen buyuk";
+    }
+
+    return 0;
+}
