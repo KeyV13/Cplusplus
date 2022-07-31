ebf769031ea22732a981ebc2e5436ffc43dad18d
Vedat <109217403+KeyV13@users.noreply.github.com>
2022-07-31 14:47:06 +0300

    Add files via upload

---
+++ Referans_Memory Address.cpp
@@ -0,0 +1,21 @@
+﻿// Referans_Memory Address.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ // Referans değişkeni, var olan bir değişkene "referanstır" ve & operatörle birlikte oluşturulur:
+	string gida = "Lahmacun"; // gıda değişkeni
+	string &yemek = gida;     // gıdaya referans
+
+	cout << gida << "\n";
+	cout << yemek << "\n";
+
+    // bir değişken oluşturulduğunda, değişkene bir bellek adresi atanır. Değişkene bir değer atadığımızda ise 
+	// bu hafıza adresine kaydedilir.
+	cout << &yemek;
+
+	return 0;
+}
